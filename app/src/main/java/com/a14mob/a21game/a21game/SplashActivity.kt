611a97175a6ac8292ebe187c0fda6e83ce4a6548
logonrm <logonrm@fiap.com.br>
2018-01-31 21:31:55 -0200

    adicionando fade i para splash

--- app/src/main/java/com/a14mob/a21game/a21game/SplashActivity.kt
+++ app/src/main/java/com/a14mob/a21game/a21game/SplashActivity.kt
@@ -1,12 +1,62 @@
 package com.a14mob.a21game.a21game
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 
 class SplashActivity : AppCompatActivity() {
+
+    private val SPLASH_DISPLAY_LENGTH = 1500L
+    private val SPLASH_DISPLAY_QTD = 2
+    lateinit var ivLogo: ImageView
+    private var vezes: Int = 0;
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        ivLogo = findViewById(R.id.ivLogo);
+        carregar(1);
+
+
+    }
+
+    /**
+     * CARREGA O SPLASH
+     */
+    fun carregar(vezes:Int) {
+        this.vezes = vezes + 1;
+
+        var animacao = AnimationUtils.loadAnimation( this, R.anim.animacao_spash);
+        animacao.reset();
+
+        ivLogo!!.clearAnimation();
+        ivLogo!!.startAnimation(animacao);
+
+        Handler().postDelayed({
+
+            if(vezes < SPLASH_DISPLAY_QTD){
+                this.carregar(this.vezes);
+            }else{
+
+                val intent = Intent(this@SplashActivity, MainActivity::class.java)
+                intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+                startActivity(intent)
+                this@SplashActivity.finish()
+
+            }
+
+        },SPLASH_DISPLAY_LENGTH)
+
+
+
+
+
+
+
     }
 }
