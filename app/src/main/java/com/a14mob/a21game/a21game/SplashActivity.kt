b4ab81376c987779fd56c519b215d86c61262561
logonrm <logonrm@fiap.com.br>
2018-01-31 19:07:40 -0200

    iniciando

---
+++ app/src/main/java/com/a14mob/a21game/a21game/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.a14mob.a21game.a21game
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
