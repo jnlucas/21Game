c80368607781ec165647dc72241847b5fdff58d7
lucas.desenv.web <lucas.desenv.web@gmail.com>
2018-02-04 17:57:30 -0200

    finalizando logica d ojogo

---
+++ app/src/main/java/com/a14mob/a21game/a21game/EstouroPontuacaoActivity.kt
@@ -0,0 +1,12 @@
+package com.a14mob.a21game.a21game
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class EstouroPontuacaoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_estouro_pontuacao)
+    }
+}
--- app/src/main/java/com/a14mob/a21game/a21game/fragments/GameFragment.kt
+++ app/src/main/java/com/a14mob/a21game/a21game/fragments/GameFragment.kt
@@ -1,14 +1,22 @@
 package com.a14mob.a21game.a21game.fragments
 
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import com.a14mob.a21game.a21game.EstouroPontuacaoActivity
 
 import com.a14mob.a21game.a21game.R
+import com.a14mob.a21game.a21game.dao.CartaDao
+import com.a14mob.a21game.a21game.model.Carta
+import kotlinx.android.synthetic.main.fragment_game.*
+import java.util.*
 
 
 /**
@@ -17,6 +25,14 @@ import com.a14mob.a21game.a21game.R
 class GameFragment : Fragment() {
 
     lateinit var tvPontuacao: TextView
+    var cartas: MutableList<Carta> = ArrayList();
+    val CartaDao = CartaDao();
+    val gerador = Random()
+    lateinit var  btProximo: Button
+    lateinit var  btRecomecar: Button
+
+
+
 
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
@@ -29,8 +45,55 @@ class GameFragment : Fragment() {
         iniciarPartida()
     }
 
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        btProximo = view!!.findViewById(R.id.btProximo);
+        this.tvPontuacao = view!!.findViewById(R.id.tvPonto)
+
+        btRecomecar = view!!.findViewById(R.id.btRecomecar);
+        
+        btRecomecar.setOnClickListener({
+            iniciarPartida();
+        })
+
+
+
+        btProximo.setOnClickListener({
+            realizaJogada();
+        })
+    }
+
     fun iniciarPartida(){
-        tvPontuacao
+        this.tvPontuacao.setText("0");
+        this.cartas = CartaDao.getBaralho().toMutableList();
+        val posicaoCartaSelecionada: Int = this.gerador.nextInt(cartas.size)
+        var cartaSelecionada: Carta = cartas.get(posicaoCartaSelecionada)
+
+        ivCarta.setImageDrawable(ContextCompat.getDrawable(context,cartaSelecionada.resouceId))
+    }
+
+    fun realizaJogada(){
+        this.cartas = CartaDao.getBaralho().toMutableList();
+        val posicaoCartaSelecionada: Int = this.gerador.nextInt(cartas.size)
+        var cartaSelecionada: Carta = cartas.get(posicaoCartaSelecionada)
+        var pontuacaoAtualizada: Int = (this.tvPontuacao.text.toString().toInt() + cartaSelecionada.pontuacao)
+
+
+        tvPontuacao.text = pontuacaoAtualizada.toString()
+
+
+        if(pontuacaoAtualizada < 21){
+            cartas.removeAt(posicaoCartaSelecionada);
+            ivCarta.setImageDrawable(ContextCompat.getDrawable(context,cartaSelecionada.resouceId))
+        }else{
+            val intent = Intent(context,EstouroPontuacaoActivity::class.java)
+            intent.putExtra("PONTUACAO",tvPontuacao.text.toString());
+            startActivity(intent)
+
+        }
+
     }
 
 }// Required empty public constructor
